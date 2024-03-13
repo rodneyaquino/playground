4b75ba33276d20a72169bd27893b0a54ff8ca786
Rodney Aquino <rodneyaquino@hotmail.com>
2024-03-13 16:08:04 -0700

    add helpers, locators and tests

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -2,6 +2,11 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class BasePage {
     protected WebDriver driver;
@@ -10,4 +15,23 @@ public class BasePage {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
+
+
+    /**
+     * Check if specified web element is present within the specified timeout
+     *
+     * @param element The WebElement to check for presence
+     * @param timeoutInSeconds The maximum time to wait for the element to be present, in seconds
+     * @return boolean Whether element is present or not
+     */
+    public boolean isElementPresent(WebElement element,  long timeoutInSeconds) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+            wait.until(ExpectedConditions.visibilityOf(element));
+            return true; // Element is present
+        } catch (TimeoutException e) {
+            return false; // Element is not present within the specified timeout
+        }
+    }
+
 }
--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,32 +1,119 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.openqa.selenium.By;
+
 
 import static utils.UrlConstants.BASE_URL;
 
 public class HomePage extends BasePage {
+    private static final Logger log = LoggerFactory.getLogger(HomePage.class);
 
-    @FindBy(css = "[href='login.php']")
-    private WebElement loginLink;
+    // ==============================================
+    // Section 1: WebElement Locators
+    // ==============================================
 
-    @FindBy(css = "[href='addauser.php']")
-    private WebElement addUserLink;
+    @FindBy(xpath = ".//button[@id='start']")
+    private WebElement playButtonLocator;
+
+    @FindBy(xpath = "//input[@type='text' and @id='number']")
+    private WebElement textInputBoxLocator;
+
+    @FindBy(xpath = ".//div[@id='endgame']")
+    private WebElement endGameOverlayLocator;
 
     public HomePage open() {
         driver.get(BASE_URL);
         return this;
     }
 
-    public void clickLogin() {
-        loginLink.click();
+
+    // ==============================================
+    // Section 2: Page Actions
+    // ==============================================
+
+    /**
+     * Method to click the Play Button
+     */
+    public void clickPlayButton() {
+        System.out.println("clickPlayButton()");
+        playButtonLocator.click();
     }
 
-    public void clickRegister() {
-        addUserLink.click();
+    /**
+     * Method to enter a number in the Text Input Box
+     */
+    public void enterNumberInTextInputBox(int number) {
+        System.out.println("enterNumberInTextInputBox()");
+        textInputBoxLocator.clear();
+        textInputBoxLocator.sendKeys(String.valueOf(number));
     }
 
-    public String getPageUrl() {
-        return driver.getCurrentUrl();
+    /**
+     * Method to click on a specific cell by its ID
+     */
+    public static void clickOnCell(WebDriver driver, String cellId) {
+        System.out.println("clickOnCell()");
+
+        // Locate the cell element by its ID
+        WebElement cell = driver.findElement(By.id(cellId));
+        cell.click();
+        System.out.println("Clicked on cell with ID: " + cellId);
     }
+
+
+    // ==============================================
+    // Section 3: Element Visibility Checks
+    // ==============================================
+
+    /**
+     * Is the Play button displayed
+     *
+     * @return boolean - true if present
+     */
+    public boolean isPlayButtonDisplayed() {
+        System.out.println("isPlayButtonDisplayed()");
+        return isElementPresent(playButtonLocator, 2);
+    }
+
+    /**
+     * Is the Text Input box displayed
+     *
+     * @return boolean - true if present
+     */
+    public boolean isTextInputBoxDisplayed() {
+        System.out.println("isTextInputBoxDisplayed()");
+        return isElementPresent(textInputBoxLocator, 2);
+    }
+
+    /**
+     * Is the End Game Overlay displayed
+     *
+     * @return boolean - true if present
+     */
+    public boolean isEndGameOverlayDisplayed() {
+        System.out.println("isEndGameOverlayDisplayed()");
+        return isElementPresent(endGameOverlayLocator, 2);
+    }
+
+
+    // ==============================================
+    // Section 4: Validation and Assertion Helpers
+    // ==============================================
+
+    /**
+     * Get the text of the End Game Overlay
+     *
+     * @return String - End Game Overlay Text
+     */
+    public String getEndGameOverlayText() {
+        System.out.println("getEndGameOverlayText()");
+        return endGameOverlayLocator.getText();
+    }
+
+
 }
